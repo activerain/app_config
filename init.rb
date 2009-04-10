ae81a91a26dabab3c7b63e37e775bc303dc773fb
Cameron Booth <cameron@cdbdesign.net>
2009-04-10 14:05:43 -0500

    In Rails 2.3 Rails.root is not a string, this fixes it

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
 require 'app_config'
 
-::AppConfig = ApplicationConfig.init(Rails.root + "/config/app_config.yml", Rails.env)
+::AppConfig = ApplicationConfig.init(Rails.root.to_s + "/config/app_config.yml", Rails.env)
